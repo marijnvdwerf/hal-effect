1a71a55de5480cedb19edc16050c56966376ae3b
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2025-02-10 01:35:00 +0100

    Add test workflow

---
+++ tests/test_basic.py
@@ -0,0 +1,8 @@
+def test_ci_works():
+    """A simple test to verify that CI is working."""
+    assert True
+
+def test_basic_math():
+    """Basic math test to verify pytest is working properly."""
+    assert 1 + 1 == 2
+    assert 2 * 3 == 6 
